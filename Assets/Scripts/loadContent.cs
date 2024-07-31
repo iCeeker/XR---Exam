608b615a41d79bb35c9da858d7f4dc9c9efe5cd6
Max <buckel20161@hs-ansbach.de>
2024-07-31 17:53:19 +0200

    Added Marker & Added Marker Logic

---
+++ Assets/Scripts/loadContent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadContent : MonoBehaviour
+{
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("Pressed");
+    }
+}
