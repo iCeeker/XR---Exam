bbf0af1dcacd853995dcb57d87a38b98602dc9ab
Max <buckel20161@hs-ansbach.de>
2024-08-01 02:34:22 +0200

    Added TextMeshPro, added Tracking UI and SceneScaleSize

---
+++ Assets/Scripts/ARController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class ARController : MonoBehaviour
+{
+    // Reference to the Scale Factor Text which displays the amount of which the content has been scaled
+    public Text scaleFactorLable;
+
+    // Initial scale Factor which has been set by the vuforia configuration
+    private float initialScaleFactor;
+
+    //  Amount, of which the objects have been scaled
+    private float currentScaleFactor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialScaleFactor = VuforiaConfiguration.Instance.Vuforia.VirtualSceneScaleFactor;
+        currentScaleFactor = initialScaleFactor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSceneScale (float scale)
+    {
+        currentScaleFactor = initialScaleFactor / scale;
+        VuforiaConfiguration.Instance.Vuforia.VirtualSceneScaleFactor = currentScaleFactor;
+
+        scaleFactorLable.text = "Skalierungsfaktor: 1 : " + currentScaleFactor;   
+    }
+
+
+}
